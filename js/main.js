2b90d07c005591708f10c17bd9a2f8ea834f5784
Daniel Chen <danielchenphhs@gmail.com>
2016-12-13 01:20:16 -0500

    structuring

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-/**
- * Created by Daniel on 12/1/16.
- */
-
-
 $(document).ready(function() {
-    $('#fullpage').fullpage();
+    $('#fullpage').fullpage({
+        sectionsColor: ['#1bbc9b', '#4BBFC3', 'gray', 'whitesmoke','whitesmoke', '#ccddff'],
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage','5thPage', 'lastPage'],
+        menu: '#menu',
+        scrollingSpeed: 500 // Change to 1000 later
+    });
 });
