ef4327daf0a5efdd85b8985c129cb8519d9cb355
Daniel Chen <danielchenphhs@gmail.com>
2016-12-14 17:02:36 -0500

    finished rough draft text

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        sectionsColor: ['#1bbc9b', '#4BBFC3', 'gray', 'whitesmoke','whitesmoke', '#ccddff'],
+        sectionsColor: ['rgba(240, 128, 128, .6)', 'rgba(154, 205, 50, .6)', 'rgba(135, 206, 250, .6)', 'rgba(255, 215, 0, .6)','rgba(192, 192, 192, .6)', 'rgba(245, 245, 245, .6)'],
         anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage','5thPage', 'lastPage'],
         menu: '#menu',
         scrollingSpeed: 500 // Change to 1000 later
     });
 });
+
+
+// lightcoral','yellowgreen', 'lightskyblue', 'gold', 'silver
