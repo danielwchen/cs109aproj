24a45d2fd2da9ac9bd76d6e89bfedbf4ab786db5
Daniel Chen <danielchenphhs@gmail.com>
2016-12-14 22:35:16 -0500

    final edits

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        sectionsColor: ['rgba(240, 128, 128, .6)', 'rgba(154, 205, 50, .6)', 'rgba(135, 206, 250, .6)', 'rgba(255, 215, 0, .6)','rgba(192, 192, 192, .6)', 'rgba(245, 245, 245, .6)'],
+        sectionsColor: ['rgba(240, 128, 128, .4)', 'rgba(154, 205, 50, .4)', 'rgba(135, 206, 250, .4)', 'rgba(255, 215, 0, .4)','rgba(192, 192, 192, .4)', 'rgba(245, 245, 245, .4)'],
         anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage','5thPage', 'lastPage'],
         menu: '#menu',
-        scrollingSpeed: 500 // Change to 1000 later
+        scrollingSpeed: 800
     });
 });
-
-
-// lightcoral','yellowgreen', 'lightskyblue', 'gold', 'silver
